70e32541f0af16f7485916fdff1f5f978d100f1b
wuta1999 <349225476@qq.com>
2022-06-28 15:20:01 +0800

    makefile的考核二

---
+++ makefile-test/test2/add.c
@@ -0,0 +1,4 @@
+int add(int d1, int d2)
+{
+	return d1+d2;	
+}
---
+++ makefile-test/test2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	int value;
+	if(argc >= 3)
+	{
+		for(i = 1; i < argc; i++)
+		{
+			value = atoi(argv[i]);
+			sum = add(sum,value);	
+		}
+		printf("test2-sum : %d\n",sum);
+	}
+	else
+	{
+		printf("command line syntax error(pattern: add n1 n2 ...)\n");
+	}
+	return 0;
+
+}
